6c8bebb283cf796c2fb2f201116b4092a136ca0f
NPNG22 <nguyendinhcaodat@gmail.com>
2023-04-14 14:12:40 -0500

    change to config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
-    },
-    api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-east-2_CZCzdCCKV", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "4g63o46c423e7b69de7205k9lh", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
